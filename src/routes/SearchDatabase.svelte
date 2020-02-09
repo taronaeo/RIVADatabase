8056871864e258660090a2b1d7b510fe73a4b0cd
Aaron Teo <aaron.teo@riv-alumni.com>
2020-02-09 14:34:31 +0800

    Continue later

---
+++ src/routes/SearchDatabase.svelte
@@ -0,0 +1,88 @@
+<script>
+  import { FirebaseApp, Collection, User, Doc } from 'sveltefire'
+
+  import Error from '../components/Error.svelte'
+  import NoMembership from '../components/NoMembership.svelte'
+  import NoPermission from '../components/NoPermission.svelte'
+
+  import firebase from 'firebase/app'
+  import 'firebase/firestore'
+  import 'firebase/analytics'
+  import 'firebase/performance'
+
+  function initializeSelect() {
+    let elems = document.querySelectorAll('select')
+    let instances = M.FormSelect.init(elems)
+  }
+</script>
+
+<svelte:head>
+  <title>RIVAlumni | Search Database</title>
+</svelte:head>
+
+<FirebaseApp { firebase } perf analytics>
+  <User let:user>
+    <Doc
+      path={ '/users/' + user.uid }
+      traceId={ 'UserDataDoc' }
+      maxWait={ 5000 }
+      let:data={ userData }
+      on:data={ () => window.setTimeout(initializeSelect, 500) }>
+
+      <div class="container">
+        { #if userData.roles.Alumni }
+          <h3>Search Database</h3>
+
+          <nav class="red">
+            <div class="nav-wrapper">
+              <div class="input-field">
+                <input id="search" type="search" placeholder="Search keywords...">
+                <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                <i class="material-icons">close</i>
+              </div>
+            </div>
+          </nav>
+
+          <div class="row valign-wrapper">
+            <div class="col s10">
+              <h5>From</h5>
+            </div>
+
+            <div class="col s2"></div>
+          </div>
+
+          <div class="row valign-wrapper">
+            <div class="col s10">
+              <select id="collection">
+                <option value="users">Users</option>
+                <option value="members">Members</option>
+                <option value="events">Events</option>
+                <option value="participations">Participations</option>
+              </select>
+            </div>
+          </div>
+
+          { #if userData.roles.Editor || userData.roles.Administrator }
+
+          { :else }
+            <NoPermission />
+          { /if }
+        { :else }
+          <NoMembership />
+        { /if }
+      </div>
+
+      <div slot="loading">
+        <div class="container">
+          <div class="progress">
+            <div class="indeterminate"></div>
+          </div>
+        </div>
+      </div>
+
+      <div slot="fallback">
+        <Error />
+      </div>
+    </Doc>
+  </User>
+</FirebaseApp>
